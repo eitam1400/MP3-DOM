18ad3e423f2d1aed5dd625fbda09bc163f806da6
eitam1400 <eitamshalom@gmail.com>
2021-09-14 02:04:35 +0300

    Update index.js
    Co-Authored-By: Cyber4sPopo <69596366+Cyber4spopo@users.noreply.github.com>

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,36 @@
  * @param {String} songId - the ID of the song to play
  */
 function playSong(songId) {
-    // Your code here
-}
+    
+} 
 
 /**
  * Creates a song DOM element based on a song object.
  */
-function createSongElement({ id, title, album, artist, duration, coverArt }) {
-    const children = []
-    const classes = []
-    const attrs = { onclick: `playSong(${id})` }
-    return createElement("div", children, classes, attrs)
-}
+ function createSongElement( id, title, album, artist, duration, coverArt ) {
+    
+    const titleEl = createElement("li", [title]);
+
+    const artistEl = createElement("li", [artist]);
+    
+    const durationEl = createElement("li", ["" + duration] ,["duration", "short-duration"], {onclick: `console.log('${duration}')`});
+  
+    const coverImageArt = [coverArt];
+    const imgEl = createElement("img", [] ,["album-art"], {src: coverImageArt});
+  
+    return createElement("ul", ["Title: ", titleEl ,"Artist: ", artistEl, "Duration: ", durationEl, imgEl],[], {id: `${id}`});
+  }
+
+  const songsList = document.getElementById('songs');
+  function elementToDOM(element){ //insert element into the DOM!
+      songsList.append(element);
+  }
+
+  player.songs.sort((a, b) =>((a.title > b.title) ? 1 : -1)); //sort songs list alphabetically by title
+
+  for(const song of player.songs){
+    elementToDOM(createSongElement(song.id, song.title, song.album, song.artist, song.duration, song.coverArt))
+  }
 
 /**
  * Creates a playlist DOM element based on a playlist object.
@@ -41,7 +59,26 @@ function createPlaylistElement({ id, name, songs }) {
  * @param {Object} attributes - the attributes for the new element
  */
 function createElement(tagName, children = [], classes = [], attributes = {}) {
-    // Your code here
+    const element = document.createElement(tagName);
+
+     // Children
+     for(const child of children) {
+        element.append(child);
+    }
+
+    // Classes
+    for(const cls of classes) {
+        element.classList.add(cls);
+    }
+    
+    // Attributes
+    for(const attr in attributes) {
+        element.setAttribute(attr, attributes[attr]);
+    }
+    return element;
 }
 
-// You can write more code below this line
+
+
+
+
